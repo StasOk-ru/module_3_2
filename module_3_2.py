bdb76a69b7324f9d7bdf66cab07152ae55d11e29
Stanislav <stasokb@gmail.com>
2024-07-06 20:40:38 +0700

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,6 +1,6 @@
 # Задача "Рассылка писем":
 
-def send_email(message, recipient, *, sender = "university.help@gmail.com"):
+def send_email(message, recipient, sender="university.help@gmail.com"):
     a = [".com", ".ru", ".net"]
     fl = 0
 
@@ -27,3 +27,15 @@ send_email('Это сообщение для проверки связи', 'vasy
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+# Пример выполняемого кода (тесты):
+# send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+# send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+# send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+# send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+# Вывод на консоль:
+# Письмо успешно отправлено с адреса university.help@gmail.com на адрес vasyok1337@gmail.com
+# НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса urban.info@gmail.com на адрес urban.fan@mail.ru
+# Невозможно отправить письмо с адреса urban.teacher@mail.uk на адрес urban.student@mail.uk
+# Нельзя отправить письмо самому себе!
